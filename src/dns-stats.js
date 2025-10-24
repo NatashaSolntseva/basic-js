02cead7b61e4a62b8b5203f47d3a9bf86b257bd7
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2025-10-24 12:53:03 +0300

    feat: add getDNSStats solution

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,20 @@ const { NotImplementedError } = require('../lib');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getDNSStats(domains) {
+  const stats = {};
+
+  domains.forEach((domain) => {
+    const parts = domain.split('.').reverse();
+    let dns = '';
+
+    parts.forEach((part) => {
+      dns += `.${part}`;
+      stats[dns] = (stats[dns] || 0) +1;
+    });
+  });
+
+  return stats;
 }
 
 module.exports = {
