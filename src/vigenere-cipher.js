4c612ed832425510aab9d6acd378d213d8673e3d
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2025-11-03 17:41:23 +0300

    feat: add vigenere solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,60 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor(direct = true) {
+    this.direct = direct;
   }
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+    let j = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const char = message[i];
+
+      if (char >= 'A' && char <= 'Z') {
+        const m = char.charCodeAt(0) - 65;
+        const k = key[j % key.length].charCodeAt(0) - 65;
+        const encryptedChar = String.fromCharCode(((m + k) % 26) + 65);
+        result += encryptedChar;
+        j++;
+      } else {
+        result += char;
+      }
+    }
+
+    return this.direct ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+    let j = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const char = message[i];
+
+      if (char >= 'A' && char <= 'Z') {
+        const m = char.charCodeAt(0) - 65;
+        const k = key[j % key.length].charCodeAt(0) - 65;
+        const decryptedChar = String.fromCharCode(((m - k + 26) % 26) + 65);
+        result += decryptedChar;
+        j++;
+      } else {
+        result += char;
+      }
+    }
+
+    return this.direct ? result : result.split('').reverse().join('');
   }
 }
 
